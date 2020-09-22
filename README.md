4655a3c6ca7a0ad4c1274e819ec0a5322b6cf68b
nalomran <nwf4321@gmail.com>
2020-09-22 12:26:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,127 @@
+# pubmed2doc
+
+## Introduction
+
+Would you like your PubMed search results to be saved as PDF or Ms Word file format? If yes, then look 
+no further, I present to you **pubmed2doc**, a python module implemented to write your PubMed search
+results to PDF or Word file. The basic workflow of the module in a nutshell is as follow:
+1. It takes your query as an input and searches it against the PubMed database. 
+2. The returned PubMed results will be then prepared in one of the two display options i.e citation or listview (based 
+on the user's choice).
+3. The prepared results will finally be written to user's supplied file choice (PDF or Word).
+
+To my knowledge, PubMed doesn't offer this feature i.e saving (or exporting) the search results to PDF or Word and 
+therefore this module provides the following advantages:
+
+1. It doesn't require any intermediate software to aid in exporting the PubMed results to PDF or Word. 
+2. It Keeps the document stored locally and can be retrieved at a later time. 
+2. The generated document is easier to read, view and edit comparing with the csv or text files exported by PubMed. 
+3. It can also be ported or incorporated easily into the user's own research resources.
+
+## Getting Started
+
+* You need to have python version >= 3.4 installed and a terminal (or similar program) to execute 
+the module through the command line. 
+
+
+* The following sections will guide you to execute the module successfully in a step-by-step fashioned.
+
+
+## Prerequisites
+
+* The following are the core Python libraries (dependencies) required for this module:
+
+  ```bash
+  biopython
+  fpdf
+  python-docx
+  ```
+
+## Install 
+1.  First, you need to download the compressed file in GitHub.
+
+2.  Uncompress it and change to the directory ```cd```:
+      ```bash
+    unzip Pubmed_To_Docs.zip
+    cd Pubmed_To_Docs/
+      ```    
+3. It is highly recommended to create conda or python virtual environment and 
+install the aforementioned dependencies within such environment (OPTIONAL):
+   * Creating conda virtual environment, please change "your_env_name" your desire environment name:
+ 
+        ```bash
+        conda create -n your_env_name
+        conda activate your_env_name
+       ```
+   
+   * Creating python virtual environment:
+ 
+       ```bash
+       pip install virtualenv
+       virtualenv your_env_name
+       source your_env_name/bin/activate
+       ```
+   
+   * **Note:** don't forget to de-activate your environment once you are completely done running the module:
+       ```bash
+       # for conda env
+       conda deactivate
+       
+       # for python env
+       deactivate
+       ```
+
+4. You may now try to install the dependencies by running the following command:
+    ```bash
+    sudo pip install -r requirements.txt
+    ```
+
+## Running the Module (pubmed2doc.py)
+
+* Please make sure you are in the same directory location as the module pubmed2doc.py, and you may try to 
+check the presence of the module by running the following command:
+    ```bash
+    ls -ltrh *.py  
+
+* #### Inputs + Options
+
+    + -q: a query to be searched against PubMed database (REQUIRED)
+
+    + -e: a user's email to access to the PubMed db (REQUIRED)
+
+    + -pdf: write PubMed results to PDF (OPTIONAL) (Default value is True)
+
+    + -word: write PubMed results to Word (OPTIONAL) (Default value is False)
+
+    + -retmax: total num of records from query to be retrieved (OPTIONAL)
+    (Default value is 20)
+
+    + -sopt: type of returned results display options (citation or listview)
+    (OPTIONAL) (Default is citation)
+  
+
+* #### Output
+    + a Word or PDF document consists of the results from PubMed according to the type of display option 
+    chosen by the user
+  
+
+* ### Command Example
+    ```bash
+    python pubmed2doc.py \
+    -q "gene expression" \
+    -e "your_email" \
+    -pdf F \
+    -word T \
+    -sopt listview
+    ```
+* The above command is just an example, you can create your own advance query and include other arguments, please see
+above sub-section "Inputs + Options"
+
+* Please note that the output file will be saved in a directory called "output"
+
+* That is it! Please check the output folder for your file
+
+## Asking for Help
+
+If you have an issue (error or bug) when executing the module or have any other enquires please feel free to send 
+me an [email](mailto:nawafalomran@hotmail.com) and I will reply as soon as I can
---
+++ pubmed2doc.py
@@ -0,0 +1,459 @@
+#!/usr/bin/env python3
+
+"""Pubmed To Documents
+
+Created on Sep, 18 2020
+
+@author: Nawaf Alomran
+
+This module allows the user to write PubMed search results to
+either Word or PDF with two display options i.e citation or listview.
+
+Please visit the README file for more information about the module and some of
+its advantages.
+
+The user needs to supply a query to search against the PubMed database and an
+email address to access the PubMed database. More arguments are also
+shown below:
+
+Input + Options
+----------------
+
+    + -q: a query to be searched against PubMed database (REQUIRED)
+
+    + -e: a user's email to access to the PubMed db (REQUIRED)
+
+    + -pdf: write PubMed results to PDF (OPTIONAL) (Default value is True)
+
+    + -word: write PubMed results to Word (OPTIONAL) (Default value is False)
+
+    + -retmax: total num of records from query to be retrieved (OPTIONAL)
+    (Default value is 20)
+
+    + -sopt: type of returned results display options (citation or listview)
+    (OPTIONAL) (Default is citation)
+
+
+
+Output
+------
+    + a Word or PDF document consists of the results from PubMed according to
+    the type of display option chosen by the user
+
+
+How to Run
+-----------
+
+    # to write to a Word file with the display option listview for the
+    query "gene expression"
+
+    python pubmed2doc2.py \
+    -q "gene expression" \
+    -e "your_email" \
+    -pdf F \
+    -word T \
+    -sopt listview
+
+    # to write to a Word file with the display option citation for the query
+    "gene expression"
+
+    python pubmed2doc2.py \
+    -q "gene expression" \
+    -e "your_email" \
+    -pdf F \
+    -word T \
+    -sopt citation
+"""
+
+import argparse
+import sys
+from pathlib import Path
+
+from Bio import Entrez  # type: ignore
+from docx import Document  # type: ignore
+from docx.shared import Pt  # type: ignore
+from fpdf import FPDF  # type: ignore
+
+
+def query_search_pubmed(query: str, ret_max: str, email: str):
+    """Search PubMed via the user's query supplied through the command line
+
+    Parameters
+    ----------
+    query: a query to be searched against PubMed database
+
+    email: a user's email to access to the PubMed database
+
+    ret_max: total number of records from query to be retrieved
+
+
+    Return
+    -------
+    retrieve document summaries as records
+
+    """
+
+    Entrez.email = email
+
+    # search the PubMed db for the entered query
+    search = Entrez.esearch(
+        db="pubmed",
+        term=query,
+        sort="relevance",
+        retmode="text",
+        retmax=ret_max,
+    )
+
+    search_records = Entrez.read(search)
+    search.close()
+
+    # get the list of ids for the searched records
+    list_ids = search_records['IdList']
+
+    print(f"\nTotal of {len(list_ids)} records found")
+
+    ids = ",".join(list_ids)
+
+    # return document summaries as a result handle
+    fetch_records = Entrez.esummary(
+        db="pubmed",
+        id=ids,
+        retmode="text",
+        rettype="medline",
+    )
+
+    search_results = Entrez.read(fetch_records)
+    fetch_records.close()
+
+    return search_results
+
+
+def records_iterator(pubmed_results):
+    """Iterates over the records returned from PubMed results
+
+    Parameters
+    ----------
+    pubmed_results: PubMed results stored in Bio.Entrez.Parser.ListElement
+
+    Return
+    -------
+    generator of the PubMed results
+
+    """
+
+    for paper in pubmed_results:
+
+        # handle Unicode Encode Error due to some unusual characters in the
+        # authors names for PDF only
+        authors = ", ".join(list(paper['AuthorList'])).encode(
+            'latin-1', 'replace').decode('latin-1')
+
+        title = paper['Title']
+
+        journal = paper['FullJournalName']
+
+        pub_date = paper['PubDate']
+
+        vol_issue = f"{paper['Volume']}({paper['Issue']}):{paper['Pages']}"
+
+        doi = f"doi: {paper['DOI']}"
+
+        pmid = f"PMID: {paper['Id']}"
+
+        # handle pmcid in case if it is unavailable
+        try:
+            pmcid = paper['ArticleIds']['pmcid'].split(";")[0]
+
+        except KeyError:
+            pmcid = 'pmc-id: un-available'
+
+        yield authors, title, journal, pub_date, vol_issue, doi, pmid, pmcid
+
+
+def write_to_pdf(pubmed_results, style_method: str) -> None:
+    """Write the PubMed results to PDF
+
+
+    Parameters
+    ----------
+    pubmed_results: PubMed results stored in Bio.Entrez.Parser.ListElement
+
+    style_method: the style to be written to a PDF file
+    (citation or listview)
+
+    Return
+    -------
+    None
+    """
+
+    pdf_doc = FPDF()
+
+    pdf_doc.add_page()
+
+    # setting the font name and size
+    pdf_doc.set_font("Arial", size=12)
+
+    # configure the header of the PDF document
+    pdf_doc.cell(200, 20, txt="PubMed Search Results", ln=1, align='C')
+
+    pdf_doc.set_display_mode(zoom='real')
+
+    records = records_iterator(pubmed_results)
+
+    for i, (authors, title, journal, *pub_info) in enumerate(records, 1):
+
+        pub_date, vol_issue, doi, pmid, pmcid = pub_info
+
+        if style_method == 'citation':
+
+            # the following add the information retrieved from the PubMed
+            # results
+            pdf_doc.multi_cell(180, 7, txt=str(i) + ': ' +
+                               authors + '. ' +
+                               title + ' ' +
+                               journal + '. ' +
+                               pub_date + ';' +
+                               vol_issue + '. ' +
+                               doi + '. ' +
+                               pmid + '; ' +
+                               pmcid + '.')
+
+            # add line break
+            pdf_doc.ln(h='2')
+
+        # selecting the other style "the listview"
+        else:
+
+            pdf_doc.multi_cell(180, 7, txt=str(i) + ': ' + "Authors: " +
+                               authors)
+
+            pdf_doc.multi_cell(180, 7, txt="Title: " + title)
+
+            pdf_doc.cell(90, 7, txt="Journal Name: " + journal,
+                         ln=1,
+                         align='L')
+
+            pdf_doc.cell(50, 7, txt="Publication Date: " +
+                         pub_date,
+                         ln=2,
+                         align='L')
+
+            pdf_doc.cell(50, 7, txt="Volume, Issue, pages: " + vol_issue,
+                         ln=2,
+                         align='L')
+
+            pdf_doc.cell(50, 7, txt=doi, ln=1, align='L')
+
+            pdf_doc.cell(50, 7, txt=pmid, ln=2, align='L')
+
+            pdf_doc.cell(50, 7, txt=pmcid, ln=2, align='L')
+
+            pdf_doc.ln(h='2')
+
+    # write the information above to MS Word file
+    print("\nwriting to a PDF file...")
+    pdf_doc.output('output/PubMed_Results.pdf')
+    print("\nDone writing.")
+
+
+def write_to_word(pubmed_results, style_method: str) -> None:
+    """Write the PubMed results to Word
+
+
+    Parameters
+    ----------
+    pubmed_results: PubMed results stored in Bio.Entrez.Parser.ListElement
+
+    style_method: the style to be written to a PDF file
+    (citation or listview)
+
+    Return
+    -------
+    None
+    """
+
+    document = Document()
+
+    # styling document (font name and size)
+    style = document.styles['Normal']
+
+    font = style.font
+    font.name = 'Arial'
+    font.size = Pt(12)
+
+    # handling header configurations
+    header = document.add_paragraph()
+    header.alignment = 1  # center the title
+    header.add_run('PubMed Search Results')  # .bold = True
+
+    records = records_iterator(pubmed_results)
+
+    for i, (authors, title, journal, *pub_info) in enumerate(records, 1):
+
+        pub_date, vol_issue, doi, pmid, pmcid = pub_info
+
+        if style_method == 'citation':
+            document.add_paragraph(str(i) + ': ' +
+                                   authors + '. ' +
+                                   title + ' ' +
+                                   journal + '. ' +
+                                   pub_date + ';' +
+                                   vol_issue + '. ' +
+                                   doi + '. ' +
+                                   pmid + '; ' +
+                                   pmcid + '.')
+
+        else:
+
+            document.add_paragraph(str(i) + ":" + "  Authors: " + authors)
+
+            document.add_paragraph("Title: " + title)
+
+            document.add_paragraph(
+                "Journal Name: " + journal)
+
+            document.add_paragraph("Publication Date: " + pub_date)
+
+            document.add_paragraph("Volume, Issue, pages: " + vol_issue)
+
+            document.add_paragraph(doi)
+
+            document.add_paragraph(pmid)
+
+            document.add_paragraph(pmcid)
+
+    # write the information above to MS Word file
+    print("\nwriting to a MS Word file...")
+    document.save('output/PubMed_Results.docx')
+    print("\nDone writing.")
+
+
+def bool_conv_args(args: str) -> bool:
+    """Convert string argument value to boolean True or False
+
+    Parameters
+    ----------
+    args: argument value to represent True or False
+
+
+    Return
+    -------
+    a converted string argument to a boolean value
+    """
+
+    # consider most possible truth values scenarios supplied by the user
+    if args.lower() in ['yes', 'true', 't', 'y']:
+        return True
+
+    elif args.lower() in ['no', 'false', 'f', 'n']:
+        return False
+
+    else:
+        raise argparse.ArgumentParser('Please make sure to enter a boolean '
+                                      'value i.e. True or False.')
+
+
+def main(args) -> None:
+    """Directs the operations and processes in this module
+
+    Parameters
+    ----------
+    args: argparse Namespace class that consists of argument-value pairs
+
+
+    Return
+    -------
+    None
+    """
+
+    query = args.query
+
+    ret_max = args.ret_max
+
+    email = args.email
+
+    style_method = args.style_opt
+
+    results = query_search_pubmed(query, ret_max, email)
+
+    # create a directory named "output" if it doesn't exists
+    if not Path("output").exists():
+        print(f"\ncreating output directory named 'output'...\n")
+        Path('output').mkdir(parents=True, exist_ok=True)
+
+    to_pdf = args.to_pdf
+
+    to_word = args.to_word
+
+    if to_pdf:
+        write_to_pdf(results, style_method)
+
+    elif to_word:
+        write_to_word(results, style_method)
+
+
+def run_command_lines() -> None:
+    """Add and parse the arguments from the command line and execute the main
+    function
+
+    Parameters
+    ----------
+    None
+
+
+    Return
+    -------
+    None
+    """
+
+    USAGE = '''write PubMed results to Word or PDF file'''
+
+    parser = argparse.ArgumentParser(description=USAGE)
+    parser.add_argument('-q',
+                        dest="query",
+                        required=True,
+                        help="a query to be searched against PubMed database")
+
+    parser.add_argument('-e',
+                        dest="email",
+                        required=True,
+                        help="a user's email to access to the PubMed db")
+
+    parser.add_argument('-pdf',
+                        dest="to_pdf",
+                        type=bool_conv_args,
+                        default=True,
+                        help="write Pubmed results to PDF (OPTIONAL) "
+                             "(Default value is True)")
+
+    parser.add_argument('-word',
+                        dest="to_word",
+                        type=bool_conv_args,
+                        default=True,
+                        help="write Pubmed results to Word (OPTIONAL) "
+                             "(Default value is False)")
+
+    parser.add_argument('-retmax',
+                        dest="ret_max",
+                        default=20,
+                        help="total num of records from query to be retrieved "
+                             "(OPTIONAL) (Default is 20)")
+
+    parser.add_argument('-sopt',
+                        dest="style_opt",
+                        default="citation",
+                        help="type of returned results style format "
+                             "(citation or listview) (OPTIONAL) "
+                             "(Default is citation)")
+
+    args = parser.parse_args()
+
+    # execute the main functions with the command line arguments
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        sys.exit('\nprogram is terminated by the user!')
+
+
+if __name__ == '__main__':
+    run_command_lines()
